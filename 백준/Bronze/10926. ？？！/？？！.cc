0532109cb0090ded9166f4c46eed88eab0703bed
jni613 <dlwndms2544@gmail.com>
2023-04-20 23:12:36 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+string ggamnol;
+
+int main()
+{
+	cin >> ggamnol;
+	cout << ggamnol << "??!";
+}
