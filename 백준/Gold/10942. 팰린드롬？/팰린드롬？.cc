9edc201198cb4667397f48e90b566b00db5cb0cc
jni613 <dlwndms2544@gmail.com>
2023-04-26 12:29:58 +0900

    [Gold IV] Title: 팰린드롬?, Time: 272 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,55 +1,63 @@
-#include<cstdio>
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<set>
-#include<map>
-#include<deque>
-#include<cmath>
-#include<string>
-#include<cstring>
-#include<algorithm>
-#include<functional>
-#include<climits>
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+
 using namespace std;
 
-int N,M;
-int arr[2000];
-int dp[2000][2000];
+int N, M;
+vector <int> num;
+int dp[2001][2001]; // a ~ b 가 팰린드롬이면 1, 아니면 0 , 초기값 INT_MAX
 
-int topdown(int l, int r){
-	if(l >= r){
-		if(l==r) dp[l][r] =1;
-		return 1;
-	}
-	if(dp[l][r]!= -1) return dp[l][r];
+int sol(int a, int b) {
+    if (dp[a][b] != INT_MAX) return dp[a][b];
+    if (num[a] != num[b]) {
+        dp[a][b] = 0;
+        return dp[a][b];
+    }
 
-	dp[l][r]=0;
-	if(arr[l] == arr[r]) dp[l][r] = topdown(l+1,r-1);
-	return dp[l][r];
+    if (a == b) {
+        dp[a][b] = 1;
+        return 1;
+    }
+    if (b-a == 1) {
+        if (num[a] == num[b]) {
+            dp[a][b] = 1;
+        }
+        return dp[a][b];
+    }
+    if (b-a == 2) {
+        if (num[a] == num[b]) {
+            dp[a][b] = 1;
+        }
+        return dp[a][b];
+    }
+    
+    dp[a][b] = sol(a+1, b-1);
+
+    return dp[a][b];
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin>>N;
-	for(int i=0;i<N;i++){
-		cin>>arr[i];
-	}
-	cin>>M;
-	memset(dp,-1,sizeof(dp));
-	int a,b;
-	for(int i=0;i<M;i++){
-		cin>>a>>b;
-		if(dp[a-1][b-1]!= -1)cout<<dp[a-1][b-1]<<"\n";
-		else cout<<topdown(a-1,b-1)<<"\n";
-	}
-	/*for(int i=0;i<N;i++){
-		for(int j=0;j<N;j++){
-			cout<<dp[i][j]<<" ";
-		}
-		cout<<"\n";
-	}*/
-}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N;
+    int x, y;
+    for (int i=0; i<N; i++) {
+        cin >> x;
+        num.push_back(x);
+        for (int j=0;j<N;j++) dp[i][j] = INT_MAX;
+    }
+    cin >> M;
+    for (int i=0; i<M; i++) {
+        cin >> x >> y;
+        int is_pal = sol(x-1, y-1);
+        if (is_pal == 1) cout << 1 << "\n";
+        else cout << 0 << "\n";
+    }
+} 
