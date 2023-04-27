3469307d3f9a920470ae05b8f91ad380cfde6db5
jni613 <dlwndms2544@gmail.com>
2023-04-27 16:39:54 +0900

    [Gold IV] Title: 빙산, Time: 68 ms, Memory: 3340 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,115 +1,128 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <tuple>
-#include <string>
-#include <string.h>
 #include <iostream>
-#include <map>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
 
 using namespace std;
 
-int N, M;
-int arr[300][300];
-int zero[300][300];
-int visit[300][300];
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { -1,1,0,0 };
+int N, M, total_ice;
+int dx[4] = {0,0,-1,1};
+int dy[4] = {1,-1,0,0};
+int sea[301][301];
+int visit[301][301];
+vector <pair<int,int>> ice;
 
-int getcount(int x, int y) {
-	int cnt = 0;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (arr[nx][ny] == 0) cnt++;
-	}
-	return cnt;
+bool range(int x, int y) {
+    if (x>=0 && y>=0 && x<N && y<M) return true;
+    return false;
 }
-int range(int x, int y) {
-	if (x >= 0 && y >= 0 && x < N && y < M && !visit[x][y])
-		return 1;
-	return 0;
+int around_water(int x, int y) {
+    int water = 0;
+    for (int d=0; d<4; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        if (range(nx, ny) && sea[nx][ny] == 0) water++;
+    }
+    return water;
 }
-void dfs(int x, int y) {
-	visit[x][y] = 1;
-	//cout << x << " " << y << "\n";
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if ( range(nx, ny) && arr[nx][ny] > 0) {
-			dfs(nx, ny);
-		}
-	}
-	return;
+void reset_visit() {
+    int nice = ice.size();
+    for (int i=0; i<nice; i++) {
+        int x = ice[i].first;
+        int y = ice[i].second;
+        visit[x][y] = 0;
+    }
+}
+int number_ice() {
+    reset_visit();
+    int nice = ice.size();
+    int partition = 0;
+
+    for (int i=0; i<nice; i++) {
+        int x = ice[i].first;
+        int y = ice[i].second;
+        if (sea[x][y] == 0 || visit[x][y]) continue;
+        partition++;
+        if (partition >= 2) break;
+
+        visit[x][y] = 1;
+        queue <pair<int,int>> q;
+        q.push({x,y});
+        while(!q.empty()) {
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+
+            for (int d=0; d<4; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                if (range(nx, ny)) {
+                    if (sea[nx][ny] > 0 && visit[nx][ny]==0) {
+                        visit[nx][ny] = 1;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+    }
+    return partition;
+}
+void melt() {
+    int nice = ice.size();
+    vector <vector<int>> melt_ice;
+    for (int i=0; i<nice; i++) {
+        int x = ice[i].first;
+        int y = ice[i].second;
+        if (sea[x][y] == 0) continue;
+        int water = around_water(x, y);
+        if (water > 0) {
+            melt_ice.push_back({x, y, water});
+        }
+    }
+    int nmelt = melt_ice.size();
+    for (int i=0; i<nmelt; i++) {
+        int x = melt_ice[i][0];
+        int y = melt_ice[i][1];
+        int melt = melt_ice[i][2];
+        sea[x][y] -= melt;
+        if (sea[x][y] <= 0) {
+            sea[x][y] = 0;
+            total_ice--;
+        }
+    }
 }
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-		}
-	}
-
-	int first = 0;
-	for (int i = 1; i <= N - 2; i++) {
-		for (int j = 1; j <= M - 2; j++) {
-			if (arr[i][j] != 0 && visit[i][j] == 0) {
-				dfs(i, j);
-				first++;
-			}
-		}
-	}
-	if (first >= 2) {
-		cout << 0;
-		return 0;
-	}
-
-	int years = 0;
-	while (true) {
-		memset(visit, 0, sizeof(visit));
-		memset(zero, 0, sizeof(visit));
-		for (int i = 1; i <= N-2; i++) {
-			for (int j = 1; j <= M-2; j++) {
-				if (arr[i][j] != 0) {
-					zero[i][j] = getcount(i, j);
-				}
-			}
-		}
-
-		for (int i = 1; i <= N-2; i++) {
-			for (int j = 1; j <= M-2; j++) {
-				arr[i][j] -= zero[i][j];
-				if (arr[i][j] < 0) arr[i][j] = 0;
-				//cout << arr[i][j] << " ";
-			}
-			//cout << "\n";
-		}
-		int numzero = 0;
-		for (int i = 1; i <= N - 2; i++) {
-			for (int j = 1; j <= M - 2; j++) {
-				if (arr[i][j] != 0) numzero++;
-			}
-		}
-
-		int island = 0;
-		for (int i = 1; i <= N-2; i++) {
-			for (int j = 1; j <= M-2; j++) {
-				if (arr[i][j] != 0 && visit[i][j] == 0) {
-					dfs(i, j);
-					island++;
-				}
-			}
-		}
-		years++;
-		if (island >= 2) break;
-		if (island < 2 && numzero == 0) {
-			cout << 0;
-			return 0;
-		}
-	}
-	cout << years;
-} 
+    cin >> N >> M;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            cin >> sea[i][j];
+            if (sea[i][j] > 0) {
+                ice.push_back({i, j});
+                total_ice++;
+            }
+        }
+    }
+    int year = 0;
+    while(1) {
+        int partition = number_ice();
+        if (total_ice == 0) {
+            cout << 0;
+            return 0;
+        }
+        if (partition >= 2) {
+            cout << year;
+            return 0;
+        }
+        melt();
+        year++;
+    }
+}
