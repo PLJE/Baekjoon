c326b6a2023862fc9cd8c49c0c99af5f7f1429cc
jni613 <dlwndms2544@gmail.com>
2023-04-28 10:51:41 +0900

    [Silver III] Title: 십자카드 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.cc
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.cc
@@ -4,52 +4,26 @@
 
 using namespace std;
 
-int num_chk[10001];
-int target, order;
-
-int get_clocknum(string s) {
-    string tmp = s.substr(0,4);
-    for (int i=1; i<=4; i++) {
-        string next_tmp = s.substr(i, 4);
-        if (tmp > next_tmp) {
-            tmp = next_tmp;
-        }
-    }
-    int to_num = stoi(tmp);
-    return to_num;
-}
-void get_num (string tmp) {
-    if (tmp.length() == 4) {
-        int clocknum = get_clocknum(tmp+tmp);
-        if (num_chk[clocknum] == 0) {
-            num_chk[clocknum] = 1;
-            order++;
-            if (clocknum == target) {
-                cout << order;
-                return;
-            }
-        }
-        return;
-    }
-
-    for (int i=1; i<=9; i++) {
-        tmp.push_back(i+'0');
-        get_num(tmp);
-        tmp.pop_back();
-    }
-}
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int x;
-    string num;
+int get_clocknum (int num) {
+    int min_val = num;
     for (int i=0; i<4; i++) {
-        cin >> x;
-        num.push_back(x+'0');
+        num = num % 1000 * 10 + num / 1000;
+        if (min_val > num) min_val = num;
     }
-    target = get_clocknum(num+num);
-    get_num("");
-    return 0;
+    return min_val;
+}
+
+int main(){
+	int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int ans = 0;
+    int target = get_clocknum(1000*a + 100*b + 10*c + d);
+    for (int i=1111; i<=target; i++) {
+        int cnum = get_clocknum(i);
+        if (cnum == i) {
+            ans++;
+        }
+        if (cnum == target) break;
+    }
+    cout << ans;
 }
