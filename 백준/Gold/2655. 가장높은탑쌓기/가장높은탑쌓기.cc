7d35d2b17fc94d29c85b1abb47ba4616a3db32a6
jni613 <dlwndms2544@gmail.com>
2023-04-29 21:41:07 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.cc
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.cc
@@ -16,13 +16,10 @@ int track[101];
 vector <vector<int>> area_weight;
 vector <int> tower;
 
-bool comp_area(vector<int> a, vector<int> b) {
-    return a[1] < b[1];
-}
 bool comp_weight(vector<int> a, vector<int> b) {
     return a[2] < b[2];
 }
-void get_order (int x) {
+void back_tracking (int x) { 
     tower.clear();
     tower.push_back(area_weight[x][0]);
     int k = track[x];
@@ -32,20 +29,6 @@ void get_order (int x) {
         k = track[k];
     }
 }
-void init() {
-    for (int i=0; i<N; i++) {
-        track[i] = -1;
-        for (int j=0; j<N; j++) {
-            dp[i][j] = 0;
-
-        }
-    }
-}
-void print() {
-    for (int i=0; i<N; i++) {
-        cout << dp[i][i]<<"\n";
-    }
-}
 int main() {
 	ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -53,19 +36,20 @@ int main() {
     
     cin >> N;
     int a, h, w;
+    track[0] = -1;
     for (int i=1; i<=N; i++) {
         cin >> a >> h >> w;
         height[i] = h;
+        track[i] = -1;
         area_weight.push_back({i, a, w}); // {idx , area, weight}
     }
     // weight 기준 정렬
-    init();
     sort(area_weight.begin(), area_weight.end(), comp_weight);
     for (int i=0; i<N; i++) {
         dp[0][i] = height[area_weight[0][0]];
     }
     int weight_ans = dp[0][0];
-    int x = 0;
+    int track_start_idx = 0;
     for (int i=1; i<N; i++) {
         int cur_area = area_weight[i][1];
         int pre_max_height = -1;
@@ -86,46 +70,12 @@ int main() {
         }
         if (weight_ans < dp[i][i]) {
             weight_ans = dp[i][i];
-            x = i;
+            track_start_idx = i;
         }
         for (int j = i+1; j<N; j++) dp[i][j] = dp[i][i];
     }
-    get_order(x);
-    // area 기준 정렬
-    init();
-    sort(area_weight.begin(), area_weight.end(), comp_area);
-    for (int i=0; i<N; i++) {
-        dp[0][i] = height[area_weight[0][0]];
-    }
-    int area_ans = dp[0][0];
-    x = 0;
-    for (int i=1; i<N; i++) {
-        int cur_weight = area_weight[i][2];
-        int pre_max_height = -1;
-        int k;
-        for (int j=i-1; j>=0; j--) {
-            if (area_weight[j][2] < cur_weight) {
-                if (pre_max_height < dp[j][i]) {
-                    pre_max_height = dp[j][i];
-                    k = j;
-                }
-            }
-        }
-        if (pre_max_height == -1) k = -1;
-        dp[i][i] += height[area_weight[i][0]];
-        if (k >= 0) {
-            dp[i][i] += dp[k][i];
-            track[i] = k;
-        }
-        if (area_ans < dp[i][i]) {
-            area_ans = dp[i][i];
-            x = i;
-        }
-        for (int j = i+1; j<N; j++) dp[i][j] = dp[i][i];
-    }
-    if (area_ans > weight_ans) {
-        get_order(x);
-    }
+    back_tracking(track_start_idx);
+
     int block_cnt = tower.size();
     cout << block_cnt << "\n";
     for (int i=block_cnt-1; i>=0; i--) cout << tower[i] << "\n";
