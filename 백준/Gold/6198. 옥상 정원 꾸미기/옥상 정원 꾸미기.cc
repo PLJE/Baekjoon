40f0ef1fb59608c5cbe68c8c41e4351a3aba1a3e
jni613 <dlwndms2544@gmail.com>
2023-05-04 00:03:35 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 44 ms, Memory: 7548 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector <int> num;
+map <int, long long> cnt;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    int x;
+    for (int i=1; i<=N; i++) {
+        cin >> x;
+        num.push_back(x);
+    }
+    stack <pair<int,int>> st;
+    st.push({num[N-1], N-1});
+
+    for (int i=N-2; i>=0; i--) {
+        int cur = num[i];
+        if (st.empty()) st.push({cur, i});
+        else {
+            int top_val = st.top().first;
+            int top_idx = st.top().second;
+            if (cur <= top_val) st.push({cur, i});
+            else {
+                while (!st.empty()) {
+                    int tmp_val = st.top().first;
+                    int tmp_idx = st.top().second;
+                    if (cur <= tmp_val) break;
+                    st.pop();
+                    cnt[i] += (cnt[tmp_idx] + 1);
+                }
+                st.push({cur, i});
+            }
+        }
+    }
+    long long ans = 0;
+    for (int i=0; i<N; i++) {
+        ans += cnt[i];
+    }
+    cout << ans;
+}
